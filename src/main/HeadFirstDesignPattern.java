93a6ad104fbb2b08c2d6715bc640c60f09af1345
tuanpnt17 <phamnguyentrongtuan13@gmail.com>
2023-12-10 09:22:55 +0000

    first commit

---
+++ src/main/HeadFirstDesignPattern.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
+to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit
+this template
+ */
+package main;
+
+/**
+ *
+ * @author TuanPNTSE173039
+ */
+public class HeadFirstDesignPattern {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
